10b26603600115a1d241753b5409652bc7b4d38a
MCrank <marco_mcse@hotmail.com>
2019-05-31 22:54:49 -0500

    Added navbar and auth code

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,96 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import {
+  BrowserRouter, Redirect, Route, Switch,
+} from 'react-router-dom';
+import AppNavbar from '../components/AppNavbar/AppNavbar';
+import Auth from '../components/pages/Auth/Auth';
+import Home from '../components/pages/Home/Home';
+import authRequests from '../helpers/data/authRequests';
+import connection from '../helpers/data/connection';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Button tag="a" color="success" size="large" href="http://reactstrap.github.io" target="_blank">
-          View Reactstrap Docs
-        </Button>
-      </header>
-    </div>
-  );
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  // props contains Location, Match, and History
+  const routeChecker = props => (authed === false ? (
+      <Component {...props} />
+  ) : (
+      <Redirect to={{ pathname: '/home', state: { from: props.location } }} />
+  ));
+  return <Route {...rest} render={props => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  // props contains Location, Match, and History
+  const routeChecker = props => (authed === true ? (
+      <Component {...props} />
+  ) : (
+      <Redirect to={{ pathname: '/auth', state: { from: props.location } }} />
+  ));
+  return <Route {...rest} render={props => routeChecker(props)} />;
+};
+
+class App extends React.Component {
+  state = {
+    authed: false,
+    pendingUser: true,
+  };
+
+  componentDidMount() {
+    connection();
+
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authed: true,
+          pendingUser: false,
+        });
+      } else {
+        this.setState({
+          authed: false,
+          pendingUser: false,
+        });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render() {
+    const { authed, pendingUser } = this.state;
+    const logoutClickEvent = () => {
+      authRequests.logoutUser();
+      this.setState({
+        authed: false,
+      });
+    };
+
+    if (pendingUser) {
+      return null;
+    }
+
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <React.Fragment>
+            <AppNavbar isAuthed={authed} logoutClickEvent={logoutClickEvent} />
+            <div className="app-content container-fluid">
+              <div className="justify-content-center">
+                <Switch>
+                  <PublicRoute path="/auth" component={Auth} authed={authed} />
+                  <PrivateRoute path="/" component={Home} authed={authed} />
+                  {/* <PrivateRoute path="/home" component={Home} authed={authed} /> */}
+                </Switch>
+              </div>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
