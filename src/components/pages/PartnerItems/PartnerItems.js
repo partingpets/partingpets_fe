f14bbc81f10bc1ef7f94073774f2a205dd674088
Colin White <clwhite814@gmail.com>
2019-06-04 21:23:50 -0500

    wip

--- src/components/pages/PartnerItems/PartnerItems.js
+++ src/components/pages/PartnerItems/PartnerItems.js
@@ -6,6 +6,10 @@ class PartnerItems extends React.Component {
         return (
             <div>
                 <h1>{partner.name}</h1>
+                <h3>{partner.street}</h3>
+                <h3>{partner.city}</h3>
+                <h4>{partner.state}</h4>
+                <h6>{partner.zipcode}</h6>
             </div>
         );
     }
