7e5a2dcc568944bb47a89d1a1b75057b2ddc1f89
Tim Harley <Timothy.D.Harley@gmail.com>
2019-06-10 21:35:29 -0500

    fleshed out the pet form modal.  Still needs implimintation

--- src/components/PetForm/PetForm.js
+++ src/components/PetForm/PetForm.js
@@ -162,6 +162,76 @@ class PetForm extends React.Component {
                   />
                 </FormGroup>
               </Col>
+              <Col md={6}>
+                <FormGroup>
+                  <Label for="burialStreet">Burial Street</Label>
+                  <Input
+                    className="form-input"
+                    type="text"
+                    name="burialStreet"
+                    id="burialStreet"
+                    placeholder="123 Street Rd"
+                    onChange={this.nameChange}
+                    value={newPet.burialStreet}
+                  />
+                </FormGroup>
+              </Col>
+              <Col md={6}>
+                <FormGroup>
+                  <Label for="burialCity">Burial City</Label>
+                  <Input
+                    className="form-input"
+                    type="text"
+                    name="burialCity"
+                    id="burialCity"
+                    placeholder="Townsville"
+                    onChange={this.nameChange}
+                    value={newPet.burialCity}
+                  />
+                </FormGroup>
+              </Col>
+              <Col md={6}>
+                <FormGroup>
+                  <Label for="burialState">Burial State</Label>
+                  <Input
+                    className="form-input"
+                    type="text"
+                    name="burialState"
+                    id="burialState"
+                    placeholder="North Hampshire"
+                    onChange={this.nameChange}
+                    value={newPet.burialState}
+                  />
+                </FormGroup>
+              </Col>
+              <Col md={6}>
+                <FormGroup>
+                  <Label for="burialZipCode">Burial Zip Code</Label>
+                  <Input
+                    className="form-input"
+                    type="text"
+                    name="burialZipCode"
+                    id="burialZipCode"
+                    placeholder=""
+                    onChange={this.nameChange}
+                    value={newPet.burialZipCode}
+                  />
+                </FormGroup>
+              </Col>
+              <Col md={6}>
+                <FormGroup>
+                  <Label for="burialPlot">Burial Plot</Label>
+                  <Input
+                    className="form-input"
+                    type="text"
+                    name="burialPlot"
+                    id="burialPlot"
+                    placeholder="A-1"
+                    onChange={this.nameChange}
+                    value={newPet.burialPlot}
+                  />
+                </FormGroup>
+              </Col>
             </Row>
             <h1>This Modal is still under construction</h1>
 
@@ -180,3 +250,5 @@ class PetForm extends React.Component {
     );
   }
 }
+
+export default PetForm;
