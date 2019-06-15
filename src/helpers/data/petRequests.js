228e961e832f82a739a588c94a85255063939f47
Tim Harley <Timothy.D.Harley@gmail.com>
2019-06-15 14:09:11 -0500

    wip

--- src/helpers/data/petRequests.js
+++ src/helpers/data/petRequests.js
@@ -12,9 +12,22 @@ const getPetsByUserId = uid => new Promise((resolve, reject) => {
     .catch(error => reject(error));
 });
 
+const getAllPets = new Promise((resolve, reject) => {
+  axios
+    .get(`${petsApiBaseUrl}/api/pets/`)
+    .then((result) => {
+      resolve(result.data);
+    })
+    .catch(error => reject(error));
+});
+
 const createPet = pet => axios.post(`${petsApiBaseUrl}/api/pets`, pet);
+
+const editPet = (petId, petObject) => axios.put(`${petsApiBaseUrl}/api/pets/update/${petId}`, petObject);
 
 export default { 
   getPetsByUserId,
+  getAllPets,
   createPet,
+  editPet,
 };
