c5ca9e5a3628e0b40f98b16dbe8e0c2db38d026a
JonathanPMohan <jonathanmohan.net@gmail.com>
2019-06-29 01:55:29 -0500

    Got Partner Admin Page Routed/Working

--- src/components/pages/Admin/Admin.js
+++ src/components/pages/Admin/Admin.js
@@ -3,22 +3,77 @@ import { NavLink as RRNavLink } from 'react-router-dom';
 import {
   UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem,
 } from 'reactstrap';
+
+import clueBkg from '../../../images/rocks.png';
+import trophy from '../../../images/trophy.png';
+import earth from '../../../images/earth.png';
+
 import './Admin.scss';
 
 class Admin extends React.Component {
+  // Change View Function //
+
+  changeView = (e) => {
+    const view = e.currentTarget.id;
+    this.props.history.push(`/${view}`);
+  };
+
   render() {
     return (
-      <div className="admin-container">
-        <h1>Admin</h1>
-        <UncontrolledDropdown>
-          <DropdownToggle>Manage</DropdownToggle>
-          <DropdownMenu>
-            <DropdownItem tag={RRNavLink} to="/partnersadmin">
-              Partners
-            </DropdownItem>
-            <DropdownItem>Users</DropdownItem>
-          </DropdownMenu>
-        </UncontrolledDropdown>
+    // <div className="admin-container">
+    //   <h1>Admin</h1>
+    //   <UncontrolledDropdown>
+    //     <DropdownToggle>Manage</DropdownToggle>
+    //     <DropdownMenu>
+    //       <DropdownItem tag={RRNavLink} to="/partnersadmin">
+    //         Partners
+    //       </DropdownItem>
+    //       <DropdownItem>Users</DropdownItem>
+    //     </DropdownMenu>
+    //   </UncontrolledDropdown>
+    // </div>
+
+      <div className="admin-home mx-auto animated bounceInLeft">
+        <h2>Welcome Parting Pets Admin. Manage Your Users, Partners, Items & Invoices</h2>
+        <div className="deck card-deck mt-5">
+          <div className="card admin-card border-dark" id="partnersadmin" onClick={this.changeView}>
+            <div className="card-body text-center">
+              <h4 className="card-title">
+                <img src={clueBkg} alt="rocks glasses" />
+              </h4>
+              <h5 className="card-subtitle mb-2 text-muted">PARTNERS</h5>
+              <p className="card-text">Manage Partners</p>
+            </div>
+          </div>
+          <div className="card admin-card border-dark" id="usersadmin" onClick={this.changeView}>
+            <div className="card-body text-center">
+              <h4 className="card-title">
+                <img src={trophy} alt="trophy" />
+              </h4>
+              <h5 className="card-subtitle mb-2 text-muted">USERS</h5>
+              <p className="card-text">Manage Users</p>
+            </div>
+          </div>
+          <div className="card admin-card border-dark" id="invoicesadmin" onClick={this.changeView}>
+            <div className="card-body text-center">
+              <h4 className="card-title">
+                <img src={earth} alt="earth" />
+              </h4>
+              <h5 className="card-subtitle mb-2 text-muted">INVOICES</h5>
+              <p className="card-text">Manage Invoices</p>
+            </div>
+          </div>
+
+          <div className="card admin-card border-dark" id="itemsadmin" onClick={this.changeView}>
+            <div className="card-body text-center">
+              <h4 className="card-title">
+                <img src={earth} alt="earth" />
+              </h4>
+              <h5 className="card-subtitle mb-2 text-muted">ITEMS</h5>
+              <p className="card-text">Manage Partner Items</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/pages/PartnersAdmin/PartnersAdmin.js
+++ src/components/pages/PartnersAdmin/PartnersAdmin.js
@@ -2,6 +2,7 @@ import React from 'react';
 import partnerRequests from '../../../helpers/data/partnerRequests';
 import PartnerItems from '../../PartnerItems/PartnerItems';
 import AddPartnerModal from '../../AddPartnerModal/AddPartnerModal';
+
 import './PartnersAdmin.scss';
 
 class PartnersAdmin extends React.Component {
@@ -11,74 +12,73 @@ class PartnersAdmin extends React.Component {
     isEditingPartner: false,
     partnerToEdit: {},
     partnerEditId: '-1',
-}
+  };
 
-getPartners = () => {
-  partnerRequests
-    .getAllPartners()
-    .then((partners) => {
-      this.setState({ partners });
-    })
-    .catch((err) => {
-      console.error('error with partners GET', err);
-    });
-};
+  getPartners = () => {
+    partnerRequests
+      .getAllPartners()
+      .then((partners) => {
+        this.setState({ partners });
+      })
+      .catch((err) => {
+        console.error('error with partners GET', err);
+      });
+  };
 
   componentDidMount() {
     this.getPartners();
   }
 
-showModal = (e) => {
+  showModal = (e) => {
     this.setState({
-        hidden: !this.state.hidden,
-        showModal: true,
+      hidden: !this.state.hidden,
+      showModal: true,
     });
-};
+  };
 
-modalCloseEvent = (e) => {
+  modalCloseEvent = (e) => {
     this.setState({
-        hidden: !this.state.hidden,
-        showModal: false,
-        partnerToEdit: {},
+      hidden: !this.state.hidden,
+      showModal: false,
+      partnerToEdit: {},
     });
-};
+  };
 
-partnerFormSubmitEvent = (newPartner) => {
+  partnerFormSubmitEvent = (newPartner) => {
     const { isEditingPartner, partnerEditId } = this.state;
     if (isEditingPartner) {
-        partnerRequests
+      partnerRequests
         .editPartner(partnerEditId, newPartner)
         .then(() => {
-                this.getPartners();
-                this.setState({
-                    showModal: false,
-                    isEditingPartner: false,
-                    partnerEditId: '-1',
-                });
-            })
-            .catch(error => console.error('There Was An Error Editing Your Partner', error));
-        } else {
-        partnerRequests
+          this.getPartners();
+          this.setState({
+            showModal: false,
+            isEditingPartner: false,
+            partnerEditId: '-1',
+          });
+        })
+        .catch(error => console.error('There Was An Error Editing Your Partner', error));
+    } else {
+      partnerRequests
         .createPartner(newPartner)
         .then(() => {
-        this.getPartners();
-        this.setState({ showModal: false });
+          this.getPartners();
+          this.setState({ showModal: false });
         })
         .catch(error => console.error('There was an error creating the partner'));
     }
-};
+  };
 
-passPartnerToEdit = partnerId => {
-    partnerRequests.getSinglePartner(partnerId)
-    .then((result) => {
-        this.setState({ 
-            isEditingPartner: true, 
-            partnerEditId: partnerId,
-            partnerToEdit: result 
-        });
-            this.showModal();
-    }); 
-}
+  passPartnerToEdit = (partnerId) => {
+    partnerRequests.getSinglePartner(partnerId).then((result) => {
+      this.setState({
+        isEditingPartner: true,
+        partnerEditId: partnerId,
+        partnerToEdit: result,
+      });
+      this.showModal();
+    });
+  };
 
   deleteOnePartner = (partnerId) => {
     partnerRequests
@@ -93,29 +93,32 @@ passPartnerToEdit = partnerId => {
       });
   };
 
-render() {
+  render() {
     const { partners, isEditingPartner, partnerToEdit } = this.state;
     const partnersComponents = partners.map(partner => (
-     <PartnerItems
+      <PartnerItems
         key={partner.id}
         partner={partner}
         deleteSinglePartner={this.deleteOnePartner}
         passPartnerToEdit={this.passPartnerToEdit}
-        />
-        ));
+      />
+    ));
     return (
-        <div className="partners-admin-container">
-            <h1>Manage The Partners</h1>
-            <button onClick={this.showModal}>ADD NEW PARTNER</button>
-            <AddPartnerModal
-                showModal={this.state.showModal}
-                onSubmit={this.partnerFormSubmitEvent}
-                modalCloseEvent={this.modalCloseEvent}
-                isEditingPartner={isEditingPartner}
-                partnerToEdit={partnerToEdit}
-            />
-            <ul>{partnersComponents}</ul>
-        </div>
+    // Test //
+
+      // Test //
+      <div className="partners-admin-container">
+        <h1>Manage The Partners</h1>
+        <button onClick={this.showModal}>ADD NEW PARTNER</button>
+        <AddPartnerModal
+          showModal={this.state.showModal}
+          onSubmit={this.partnerFormSubmitEvent}
+          modalCloseEvent={this.modalCloseEvent}
+          isEditingPartner={isEditingPartner}
+          partnerToEdit={partnerToEdit}
+        />
+        <ul>{partnersComponents}</ul>
+      </div>
     );
   }
 }
