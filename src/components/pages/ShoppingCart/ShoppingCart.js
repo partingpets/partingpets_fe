6684213f4e984d9f14b7443713eaa82afe595e22
JonathanPMohan <jonathanmohan.net@gmail.com>
2019-07-03 08:40:07 -0500

    working on styling the cart

--- src/components/pages/ShoppingCart/ShoppingCart.js
+++ src/components/pages/ShoppingCart/ShoppingCart.js
@@ -7,9 +7,11 @@ import orderRequests from '../../../helpers/data/orderRequests';
 import CartItem from '../../CartItem/CartItem';
 import './ShoppingCart.scss';
 
+import pets from '../../AppNavbar/images/pets_small.png';
+
 const defaultOrder = {
   userId: 0,
-  paymentTypeId: 0, 
+  paymentTypeId: 0,
   orderLines: [],
 };
 
@@ -81,29 +83,28 @@ class ShoppingCart extends React.Component {
   formSubmit = (e) => {
     e.preventDefault();
     const { userObject } = this.props;
-    const tempCart = this.state.cart 
+    const tempCart = this.state.cart;
     const myNewOrder = { ...this.state.newOrder };
     myNewOrder.userId = userObject.id;
     myNewOrder.paymentTypeId = 4;
     tempCart.forEach((cartOrder) => {
-      let tempObject = {};
+      const tempObject = {};
       tempObject.productID = cartOrder.productId;
       tempObject.quantity = cartOrder.quantity;
       myNewOrder.orderLines.push(tempObject);
     });
-    orderRequests.createOrder(myNewOrder)
-    .then((result) => {
-      if(result.status === 201) {
+    orderRequests.createOrder(myNewOrder).then((result) => {
+      if (result.status === 201) {
         alert("We're sorry for your loss, but congratulations on your purchase!");
         this.state.cart.forEach((item) => {
           this.deleteCartItem(item.cartId);
-        })
+        });
       }
     });
     this.setState({
       newOrder: defaultOrder,
     });
-  }
+  };
 
   deleteCartItem = (itemId) => {
     const { userObject, updateCartBadge } = this.props;
@@ -134,9 +135,10 @@ class ShoppingCart extends React.Component {
       <div className="shoppingCart">
         <Container className="cart-container">
           <Row className="cart-header-row">
+            <img src={pets} className="petsCartLogo" alt="pets_logo" />
             <Col>
-              <h1>Here's what's in your Shopping cart</h1>
-              <h5>Get free shipping on all orders.</h5>
+              <h1>Here's what's in your Parting Pets Shopping cart</h1>
+              <h5>Get free shipping on all Parting Pets orders.</h5>
             </Col>
           </Row>
           {cartItemComponent(cart)}
@@ -176,8 +178,13 @@ class ShoppingCart extends React.Component {
               </Col>
             </Row>
           </div>
-          <div className="cart-checkout-btn">
-            <Button color="primary" onClick={this.formSubmit}>Check Out</Button>
+          <div className="checkout cart-checkout-btn2">
+            <button className="cart-checkout-btn" onClick={this.formSubmit}>
+              <span className="spot">
+                <span className="right-arrow lnr lnr-arrow-right-circle" />
+                CHECK OUT
+              </span>
+            </button>
           </div>
         </Container>
       </div>
