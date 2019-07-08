b0293c674e039e272d1ae68e6090bb5a7d2654e3
Colin White <clwhite814@gmail.com>
2019-07-07 20:58:45 -0500

    WIP

--- src/components/OrderItems/OrderItems.js
+++ src/components/OrderItems/OrderItems.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Button } from 'reactstrap';
 import './OrderItems.scss';
+import utility from '../../helpers/utils/utility';
 
 class OrderItems extends React.Component
 {
@@ -8,14 +10,34 @@ class OrderItems extends React.Component
 
         return (
             <div className="orderCard card2 col-6">
-                <h1>Details for Order #{order.id}</h1>
-                <h3>{order.firstName} {order.lastName}</h3>
-                {order.orderItems.map((orderitem, i) => (
-                    //<div>
+                <h2>Details for Order #{order.id}</h2>
+                <h3>Ordered on {utility.dateFormat(order.purchaseDate)}</h3>
+
+                <div className="billing-ordersummary">
+                
+                <div className="billing-address">
+                <h4>Billing Address</h4>
+                <h6>{order.firstName} {order.lastName}</h6>
+                <h6>{order.street1}</h6>
+                <h6>{order.city}, {order.state} {order.zipCode}</h6>
+                <h5 className="payment-method">Payment Method: VISA</h5>
+                </div>
+            
+                <div className="order-summary">
+                <h4>Order Summary</h4>
+                <h6>Item(s) Subtotal: ${order.subtotal}</h6>
+                <h6>Shipping & Handling: $0.00</h6>
+                <h6>Sales Tax: ${order.tax}</h6>
+                <h6>Grand Total: ${order.total}</h6>
+                <Button color="primary">More Details</Button>
+                </div>
+
+                </div>
+                {/* <h5>Payment Method: VISA</h5> */}
+
+                {/* {order.orderItems.map((orderitem, i) => (
                     <h3 key={i}>{orderitem.name}</h3>
-                    //<h3>{orderitem.quantity}</h3>
-                    // </div>
-                ))}
+                ))} */}
             </div>
         );
     }
