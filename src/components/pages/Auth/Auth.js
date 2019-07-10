35e70e5bf646a16295e11c334219878e10a1bdb4
JonathanPMohan <jonathanmohan.net@gmail.com>
2019-07-09 20:16:29 -0500

    reworked auth/added logo, removed navbar

--- src/components/pages/Auth/Auth.js
+++ src/components/pages/Auth/Auth.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import { GoogleLoginButton } from 'react-social-login-buttons';
+import { GoogleLoginButton, createButton } from 'react-social-login-buttons';
 import authRequests from '../../../helpers/data/authRequests';
 import './Auth.scss';
+
+import petsLogo from '../../../images/pets.png';
+
+const btnConfig = {
+  text: 'Google Login',
+  icon: 'paw',
+  iconFormat: name => 'fas fa-paw',
+  style: { background: '#15153a' },
+  activeStyle: { background: '#293e69' },
+};
+/** My Google login button. */
+const MyGoogleLoginButton = createButton(btnConfig);
 
 class Auth extends React.Component {
   state = {
@@ -20,8 +32,12 @@ class Auth extends React.Component {
   render() {
     return (
       <div className="Auth mt-5">
+        <div className="homeLogo">
+          <img src={petsLogo} className="petsHomeLogo ml-auto" alt="pets_logo" />
+        </div>
         <div className="login d-flex justify-content-center">
-          <GoogleLoginButton id="googleBtn" onClick={this.googleAuthenticateUser} />
+          {/* <GoogleLoginButton id="googleBtn" onClick={this.googleAuthenticateUser} /> */}
+          <MyGoogleLoginButton id="googleBtn" onClick={this.googleAuthenticateUser} />
         </div>
       </div>
     );
