7a923bd547289b53d350fa47314f3046da7b180a
Colin White <clwhite814@gmail.com>
2019-07-10 21:18:11 -0500

    wip

--- src/components/OrderItems/OrderItems.js
+++ src/components/OrderItems/OrderItems.js
@@ -1,48 +1,117 @@
-import React from 'react';
-import { Button } from 'reactstrap';
-import './OrderItems.scss';
-import utility from '../../helpers/utils/utility';
+import React from "react";
+import { Collapse, Button, CardBody, Card, Row, Col } from "reactstrap";
+import "./OrderItems.scss";
+import utility from "../../helpers/utils/utility";
 
-class OrderItems extends React.Component
-{
+class OrderItems extends React.Component {
+  state = {
+    collapse: false
+  };
 
-    orderClick = () => {
-        const { order, onSelect } = this.props;
-        onSelect(order.id);
-        console.log(order.id);
-    }
+  // orderClick = () => {
+  //     const { order, onSelect } = this.props;
+  //     onSelect(order.id);
+  //     console.log(order.id);
+  // }
 
-    render() {
-        const { order } = this.props;
+  // toggle = () => {
+  //     this.setState({ collapse: !collapse });
+  // }
 
-        return (
-            <div className="orderCard card2 col-6">
-                <h2 className="order-details-header">Details for Order #{order.id}</h2>
-                <h3>Ordered on {utility.dateFormat(order.purchaseDate)}</h3>
+  toggle = () => {
+    this.setState({
+      collapse: !this.state.collapse
+    });
+  };
 
-                <div className="billing-ordersummary">
-                
-                <div className="billing-address">
+  render() {
+    const { order } = this.props;
+
+    return (
+      <div className="orderCard card2 col-6">
+        <Row>
+          <Col className="col-sm-8">
+            <Row>
+              <h2 className="order-details-header">
+                Details for Order #{order.id}
+              </h2>
+            </Row>
+            <Row>
+              <h3>Ordered on {utility.dateFormat(order.purchaseDate)}</h3>
+            </Row>
+            <div className="billing-address">
+              <Row>
                 <h4>Billing Address</h4>
-                <h6>{order.firstName} {order.lastName}</h6>
+              </Row>
+              <Row>
+                <h6>
+                  {order.firstName} {order.lastName}
+                </h6>
+              </Row>
+              <Row>
                 <h6>{order.street1}</h6>
-                <h6>{order.city}, {order.state} {order.zipCode}</h6>
-                <h5 className="payment-method">Payment Method: {order.payment}</h5>
-                </div>
-            
-                <div className="order-summary">
-                <h4>Order Summary</h4>
-                <h6>Item(s) Subtotal: ${order.subtotal}</h6>
-                <h6>Shipping & Handling: $0.00</h6>
-                <h6>Sales Tax: ${order.tax}</h6>
-                <h6 className="grand-total">Grand Total: ${order.total}</h6>
-                <Button color="primary" onClick={this.orderClick}>More Details</Button>
-                </div>
-
-                </div>
+              </Row>
+              <Row>
+                <h6>
+                  {order.city}, {order.state} {order.zipCode}
+                </h6>
+              </Row>
+              <Row>
+                <h5 className="payment-method">
+                  Payment Method: {order.payment}
+                </h5>
+              </Row>
             </div>
-        );
-    }
+          </Col>
+          <Col className="order-sum col-sm-4">
+            <div className="order-summary">
+              <Row>
+                <h4>Order Summary</h4>
+              </Row>
+              <Row>
+                <h6>Item(s) Subtotal: ${order.subtotal}</h6>
+              </Row>
+              <Row>
+                <h6>Shipping & Handling: $0.00</h6>
+              </Row>
+              <Row>
+                <h6>Sales Tax: ${order.tax}</h6>
+              </Row>
+              <Row>
+                <h6 className="grand-total">Grand Total: ${order.total}</h6>
+              </Row>
+              <Row>
+                <Button
+                  color="primary"
+                  onClick={this.toggle}
+                  style={{ marginBottom: "1rem" }}
+                >
+                  More Details
+                </Button>
+              </Row>
+            </div>
+          </Col>
+        </Row>
+
+        <Row>
+          <Collapse isOpen={this.state.collapse}>
+              <hr/>
+            <Card>
+              <CardBody>
+                {order.orderItems.map((orderitem, i) => (
+                    <div>
+                    <h4>{orderitem.quantity}</h4>
+                    <h4 key={i}>{orderitem.name}</h4>
+                    <h4>{orderitem.unitPrice}</h4>
+                  </div>
+                ))}
+              </CardBody>
+            </Card>
+          </Collapse>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default OrderItems;
--- src/components/pages/OrderDetail/OrderDetail.js
+++ src/components/pages/OrderDetail/OrderDetail.js
@@ -1,26 +1,31 @@
 import React from 'react';
+import orderRequests from '../../../helpers/data/orderRequests';
 import './OrderDetail.scss';
 
 class OrderDetail extends React.Component {
-// state = {
-//     singleOrder: {},
-// };
+state = {
+    singleOrder: {},
+};
 
-// backToOrdersView = (e) => {
-//     this.props.history.push(`/orders`);
-// };
+backToOrdersView = (e) => {
+    this.props.history.push(`/orders`);
+};
 
-// componentDidMount() {
-//     // const { id } = this.props.match.params;
-
-// }
+componentDidMount() {
+    const { id } = this.props.match.params;
+    orderRequests.getOrderByOrderId(id)
+    .then((singleOrder) => {
+        this.setState({ singleOrder });
+    });
+}
 
 render() {
-   //  const { singleOrder } = this.state;
+   const { singleOrder } = this.state;
 
     return (
         <div>
             <h1 className="order-detail-header">Orders Details</h1>
+            <h3>{singleOrder.firstName}</h3>
         </div>
     );
 }
