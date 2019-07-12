fd6b66cb8c780142e8f8e9f9307e0ee23b9dc0a1
JonathanPMohan <jonathanmohan.net@gmail.com>
2019-07-12 17:28:27 -0500

    home page is presentable, needs tweaking

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -9,6 +9,8 @@ import marco from '../../../images/marco_pets.png';
 import jonathan from '../../../images/jon_pets.png';
 import colin from '../../../images/colin_pets.png';
 import tim from '../../../images/Tim_pets.png';
+import petco from '../../../images/petco_logo.png';
+import petsmart from '../../../images/pet_smart.png';
 
 class Home extends React.Component {
   state = {
@@ -89,7 +91,7 @@ class Home extends React.Component {
     return (
       <div className="home-page animated fadeIn">
         <div className="home-info justify-content-center" />
-        <div className="card-home home-page">
+        <div className="card-home home-page ml-auto">
           <div className="card-body-home">
             <img src={petsLogo} className="pets-home-logo ml-auto" alt="pets_logo" />
             <hr />
@@ -102,15 +104,17 @@ class Home extends React.Component {
               services, advice and experiences that keep us making them benjamins. We aim to remain socially wealthy and
               emotionally rich AF. Everything we do is guided by our razor sharp vision: Dead Pets + Sad Owners =
               $$$Dollar Dollar Bills Yall$$$ Parting Pets employees more than 26,000 partners and operates more than
-              1,500 Parting Pets locations across the U.S., Mexico and Puerto Rico. The Parting Pets Foundation, an
-              independent nonprofit organization that we aren't afiliated with at all because they are a shell company
-              for a money laundering scheme, has invested more than $250 trillion since it was created in 2019 to help
-              promote and improve the quality of pet funerals. In conjunction with the Foundation, we work with and
-              support thousands of local animal welfare groups across the country and, through in-store adoption events,
-              help find homes for more than 400,000 animals every year, but mostly just a lot of fish.{' '}
+              1,500 Parting Pets locations across the U.S., Mexico, Puerto Rico and undisclosed parts of Cuba. The
+              Parting Pets Foundation, an independent nonprofit organization that we aren't afiliated with at all
+              because they are a shell company for a money laundering scheme, has invested more than $250 trillion since
+              it was created in 2019 to help promote and improve the quality of pet funerals. In conjunction with the
+              Foundation, we work with and support thousands of local animal welfare groups across the country and,
+              through in-store adoption events, help find homes for more than 400,000 animals every year, but mostly
+              just a lot of really stupid fish.{' '}
             </h5>
             <hr />
             <h2 className="card-title">MEET THE BRAVE, BOLD MEN BEHIND PARTING PETS SUCCESS</h2>
+            <hr />
             <div className="who-we-are-wrap">
               <div className="who-we-are-container">
                 <div className="card-who-we-are">
@@ -119,8 +123,8 @@ class Home extends React.Component {
                     <h4 className="card-text-who">Money Marco </h4>
                     <h5 className="card-text-who">PRESIDENT/CEO </h5>
                     <h6 className="card-text-who">
-                      Marco serves as our CEO. He also likes to surf the web late at night in search of the finest deals
-                      across the land.{' '}
+                      Marco serves as our fearless CEO. He also likes to surf the web really late at night in search of
+                      the finest "deals" across the land.{' '}
                     </h6>
                   </div>
                 </div>
@@ -131,8 +135,8 @@ class Home extends React.Component {
                     <h4 className="card-text-who">CW Bucks </h4>
                     <h5 className="card-text-who">CASKETS & URNS </h5>
                     <h6 className="card-text-who">
-                      Marco serves as our CEO. He also likes to surf the web late at night in search of the finest deals
-                      across the land.{' '}
+                      CW Bucks is our casket man. This dude knows caskets and urns like the back of his wallet. He also
+                      knows how to stack that cash.{' '}
                     </h6>
                   </div>
                 </div>
@@ -143,8 +147,8 @@ class Home extends React.Component {
                     <h4 className="card-text-who">Tim Dogg </h4>
                     <h5 className="card-text-who">PET FUNERALS </h5>
                     <h6 className="card-text-who">
-                      Marco serves as our CEO. He also likes to surf the web late at night in search of the finest deals
-                      across the land.{' '}
+                      You want Spuds McKenzie at Rovers funeral, Tim's your man. Looking to land the Taco Bell dog as a
+                      palbearer? Call Tim Dogg.{' '}
                     </h6>
                   </div>
                 </div>
@@ -155,9 +159,20 @@ class Home extends React.Component {
                     <h4 className="card-text-who">Jon E. Banks </h4>
                     <h5 className="card-text-who">REVENGE 4 HIRE </h5>
                     <h6 className="card-text-who">
-                      Marco serves as our CEO. He also likes to surf the web late at night in search of the finest deals
-                      across the land.{' '}
+                      Neighbor poison your pet because it keep pooping on his lawn? Call Jon. E. Banks. Revenge for hire
+                      has never been cleaner or cheaper.{' '}
                     </h6>
+                  </div>
+                </div>
+                <hr />
+                <div className="partners-wrap">
+                  <h1 className="partner-title">MEET A FEW OF OUR PARTING PETS PARTNERS</h1>
+                  <div className="who-we-are-container">
+                    <img src={petco} className="pets-partner-logo " alt="petco_logo" />
+
+                    <img src={petsmart} className="pets-partner-logo " alt="petco_logo" />
+
+                    <img src={petco} className="pets-partner-logo " alt="petco_logo" />
                   </div>
                 </div>
               </div>
