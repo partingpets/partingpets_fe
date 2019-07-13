f11d6e2a24c4e28ddc2d718ca99be649985e0975
JonathanPMohan <jonathanmohan.net@gmail.com>
2019-07-13 12:50:34 -0500

    tweaking

--- src/components/Payments/Payments.js
+++ src/components/Payments/Payments.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import './Payments.scss';
+import {
+  Button, Form, FormGroup, Label, Input, Row, Col,
+} from 'reactstrap';
 import paymentRequests from '../../helpers/data/paymentRequests';
 import PaymentOptions from './PaymentOptions';
 import PaymentModal from './PaymentModal';
-import {Button, Form, FormGroup, Label, Input, Row, Col} from 'reactstrap';
-
 
 class Payments extends React.Component {
-
   state = {
     calledPaymentOptionsInState: [],
     paymentModal: false,
     selectedPaymentId: -1,
     isEditingPayment: false,
     paymentIdToEdit: -1,
-  }
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     this.getPaymentOptions();
   }
 
   getPaymentOptions() {
-    const userId = this.props.userId;
+    const { userId } = this.props;
     paymentRequests.getPaymentOptions(userId).then((paymentOptions) => {
       this.setState({
         calledPaymentOptionsInState: paymentOptions,
@@ -45,10 +45,10 @@ class Payments extends React.Component {
 
   formFieldStringState = (event) => {
     event.preventDefault();
-    const {paymentIdCallBack} = this.props;
+    const { paymentIdCallBack } = this.props;
     let tempPayment = this.state.selectedPaymentId;
     tempPayment = event.target.value === 'Payment Methods' ? -1 : event.target.value;
-    paymentIdCallBack(tempPayment)
+    paymentIdCallBack(tempPayment);
     this.setState({
       selectedPaymentId: tempPayment,
     });
@@ -58,11 +58,11 @@ class Payments extends React.Component {
 
   paymentFormSubmitEvent = (payment) => {
     const { isEditingPayment, paymentIdToEdit } = this.state;
-    if (isEditingPayment){
+    if (isEditingPayment) {
       paymentRequests
         .editPaymentOption(paymentIdToEdit, payment)
         .then(this.getPaymentOptions())
-        .then(this.setState({ isEditingPayment: false, paymentIdToEdit: -1 }))
+        .then(this.setState({ isEditingPayment: false, paymentIdToEdit: -1 }));
     } else {
       paymentRequests.createPaymentOption(payment).then(this.getPaymentOptions());
     }
@@ -77,50 +77,51 @@ class Payments extends React.Component {
         this.getPaymentOptions();
       })
       .catch(error => console.error('error with deleting this payment option', error));
-  }
+  };
 
-  render(){
+  render() {
     const { isProfilePage } = this.props;
 
-    const { 
-      calledPaymentOptionsInState, 
-      selectedPaymentId, 
-      isEditingPayment, 
-      paymentIdToEdit,
+    const {
+      calledPaymentOptionsInState, selectedPaymentId, isEditingPayment, paymentIdToEdit,
     } = this.state;
 
     const paymentOptions = paymentOption => (
-      <PaymentOptions 
+      <PaymentOptions
         key={paymentOption.id}
         paymentOption={paymentOption}
         deleteThisPayment={this.deletePayment}
         passPaymentToEdit={this.passPaymentToEdit}
         toggle={this.toggle}
       />
-    )
+    );
 
     const paymentSelector = paymentOption => (
-      <option key={paymentOption.id} paymentoption={paymentOption} value={paymentOption.id}>{paymentOption.name}</option>
-    )
+      <option key={paymentOption.id} paymentoption={paymentOption} value={paymentOption.id}>
+        {paymentOption.name}
+      </option>
+    );
 
-    const listedPaymentOptions = calledPaymentOptionsInState.map(paymentOptions)
+    const listedPaymentOptions = calledPaymentOptionsInState.map(paymentOptions);
 
-    const selectorPaymentOptions = calledPaymentOptionsInState.map(paymentSelector)
+    const selectorPaymentOptions = calledPaymentOptionsInState.map(paymentSelector);
 
     const listOrSelector = () => {
       if (isProfilePage) {
-        return( 
-          <div>
-            {listedPaymentOptions}
-          </div>
-        )
-      } else return(
+        return <div>{listedPaymentOptions}</div>;
+      } return (
           <Form>
             <Row>
               <Col md={3}>
                 <FormGroup>
                   <Label for="paymentSelect">Select Payment Method</Label>
-                  <Input onChange={this.selectedPaymentChange} type="select" name="select" id="select" value={selectedPaymentId}>
+                  <Input
+                    onChange={this.selectedPaymentChange}
+                    type="select"
+                    name="select"
+                    id="select"
+                    value={selectedPaymentId}
+                  >
                     <option>Payment Methods</option>
                     {selectorPaymentOptions}
                   </Input>
@@ -128,13 +129,13 @@ class Payments extends React.Component {
               </Col>
             </Row>
           </Form>
-        )
-    }
+      );
+    };
 
-    return(
+    return (
       <div>
         {listOrSelector()}
-        <PaymentModal 
+        <PaymentModal
           isOpen={this.state.paymentModal}
           toggle={this.toggle}
           onSubmit={this.paymentFormSubmitEvent}
@@ -144,7 +145,7 @@ class Payments extends React.Component {
         />
         <Button outline size="sm" className="addPaymentButton" onClick={this.toggle}>
           <i className="add-payment-icon lnr lnr-plus-circle" />
-          Add a new payment method
+          Add New Payment
         </Button>
       </div>
     );
